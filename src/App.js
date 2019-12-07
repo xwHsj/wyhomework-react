a311bca578eac3b0b7c7af1e2e05c6b851b895d0
HeShengJie <906753369@qq.com>
2019-12-07 21:44:59 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,63 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component, useState  } from 'react';
+import InputNumber from './components/InputNumber'
 import logo from './logo.svg';
 import './App.css';
 
-class AppA extends Component{
-  state = {
-    num: 1
-}
-render() {
-    return (
-        <div>
-            sss
-        </div>
-    );
-}
-componentDidUpdate(prevProps, prevState) {
-    console.log(this.state.num)
-}
-componentDidMount() {
-    setTimeout(() => {
-        console.log('timeout')
-    })
-    new Promise((resovle) => {
-        console.log('before')
-        resovle('then')
-        console.log('after')
-    }).then(res => {
-        console.log(res)
-    })
-    let {num} = this.state
-    this.setState({
-        num: ++num
-    }, () => {
-        console.log('after setState')
-    })
-    console.log('---')
-}
-}
-
-function App() {
+function App(){
+  const [value,setValue] = useState('aaa')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
-      <AppA></AppA>
-    </div>
-  );
+      <div>
+      <InputNumber value={value} onChange={e=>{}}/>
+      <InputNumber defaultValue={value} onChange={e=>{}}/>
+      </div>
+  )
 }
 
-export default App;
+export default App
